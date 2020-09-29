31f16039d61988f9f815d452d0140878110d1c6a
Lepekhina-Anastasia <63790793+Lepekhina-Anastasia@users.noreply.github.com>
2020-09-29 18:39:52 +0300

    Add files via upload

--- PrintBook.h
+++ PrintBook.h
@@ -1,40 +1,27 @@
-#pragma once
-#indef book_h
+#ifndef pbook_h
 #define pbook_h
 #include <iostream>
 #include <string>
 #include "Book.h"
 using namespace std;
-class PrintBook :
-	public Book {
-private:
-	int printPages;
-public:
-	PrintBook() : Book() {
-		printPages = 0;
-	}
-	int getPrintPages() {
-		return printPages;
-	}
-	void setName(string s) {
-		Name = s;
-	}
-	string Name;
-	int Pages;
-	Book() {
-		Name = '-';
-		Pages = 0;
-	}
-	string getName() {
-		return Name;
-	}
-	string getPages() {
-		return Pages;
-	}
-	void Print() {
-		cout << "Book: " << Name << ",p." << Pages << '.';
-	}
-};
 
+class PrintBook:public Book{
+	private:
+		int printPages;
+	public:
+		PrintBook():Book(){
+			printPages=0;
+		}
+		int getPrintPages(){
+			return printPages;
+		}
+		void setName(string s){
+			Name = s;
+		}
+		void setPages(int p){
+			Pages = p;
+			printPages = p/16;
+		}
+}; 
 
-
+#endif
